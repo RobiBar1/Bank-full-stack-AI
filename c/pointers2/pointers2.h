e0b33c408f4b17fbbfb7cd3edf1a6ce9f1467d8d
reuven.bar <1robib1@gmail.com>
2025-12-09 12:03:43 +0200

    Remove poinetrs  files from String  branch

--- c/pointers2/pointers2.h
+++
@@ -1,9 +0,0 @@
-#ifndef __POINTERS2_H__
-#define __POINTERS2_H__
-
-int ISPalindrome(const char* str);
-void SevenBoom(long from, long to);
-char* RemoveWhiteSpaces(char* str);
-char* LongAddition(const char* num1, const char* num2, char* out_res); /* "alex-advance" */
-
-#endif /*__POINTERS2_H__*/
