a986debace687232712fcad7e0a8586995e26400
reuven.bar <1robib1@gmail.com>
2025-12-10 23:23:46 +0200

    moved files in the string directory

---
+++ c/pointers2/String/String.c
@@ -0,0 +1,373 @@
+#include <stddef.h> /* size_t type */
+#include <assert.h> /* for asserts use */
+#include <stdlib.h> /* Required for malloc() and free() */
+
+size_t StrLen(const char *str);
+char* StrCpy(char* dst, char* src);
+
+char* StrTok(char* str, const char* delim)
+{
+	static char* last_str = NULL;
+	static size_t last_index;
+	size_t i, deli_index, out_index = 0;
+	int found_delimiter = 0;
+	
+	assert(NULL != delim || (NULL != str && NULL == last_str));
+	
+	if (NULL == last_str || str != last_str)
+	{
+		last_str = str;
+		i = 0;
+	}
+	else
+	{
+		i = last_index;
+	}
+	if ('\0' != str[i])
+	{
+		return NULL;
+	}
+	
+	out_index = i;	
+	while ('\0' != str[i])
+	{
+		for (deli_index = 0; '\0' != delim[deli_index]; ++deli_index)
+		{
+			if (delim[deli_index] == str[i])
+			{
+				last_index = i + 1;
+				found_delimiter++;
+				break;
+			}
+		}
+		
+		if (found_delimiter)
+		{
+			break;
+		}
+		
+		i++;
+	}
+	
+	return &str[out_index];
+}
+
+size_t StrSpn(const char* str, const char* accept)
+{
+	size_t str_i, accept_i, out_count_init_equals_chars = 0, is_init = 0;
+	
+	for (str_i = 0; '\0' != str[str_i]; ++str_i)
+	{
+		for (accept_i = 0; '\0' != accept[accept_i]; ++accept_i)
+		{
+			if (str[str_i] == accept[accept_i])
+			{
+				out_count_init_equals_chars++;
+				is_init++;
+				break;	
+			}
+		}
+		
+		if (is_init)
+		{
+			is_init = 0;
+			continue;
+		}
+		
+		break;
+	}
+	
+	return out_count_init_equals_chars;
+}
+
+char* StrStr(const char* haystack, const char* needle)
+{
+	size_t stack_index, needle_index, stack_size;
+	
+	assert(NULL != haystack && NULL != needle);
+	
+	stack_size = StrLen(haystack);
+	if ('\0' == needle[0])
+	{
+		return 	(char*)haystack;
+	}
+	
+	for (stack_index = 0, needle_index = 0; stack_index < stack_size - 1; ++stack_size)
+	{
+		if(haystack[stack_index] == needle[needle_index])
+		{
+			needle_index++;
+			if('\0' == needle[needle_index])
+			{
+				stack_index++;
+				return (char*)&haystack[stack_index - needle_index];
+			}
+		}
+		else
+		{
+			needle_index = 0;
+		}
+	}
+	
+	return NULL;
+}
+
+char* StrNCat(char* dst, const char* src, size_t n_bytes)
+{
+	char* out_arr;
+	size_t i, j, out_arr_size, left_size, right_size;
+	
+	assert(NULL != dst && NULL != src );
+	
+	left_size = StrLen(dst);
+	right_size = StrLen(src);
+	out_arr_size =  left_size + right_size + 1;
+	out_arr = (char*)malloc(sizeof(char) * out_arr_size);
+	
+	for (i = 0; i < left_size && i < n_bytes; ++i)
+	{
+		out_arr[i] = dst[i];
+	}
+	
+	for (j = 0; i < right_size && i < n_bytes; ++j, ++i)
+	{
+		out_arr[i] = src[j];
+	}
+	
+	while('\0' != out_arr[i])
+	{
+		out_arr[i] = '\0';		
+	}
+	
+	return out_arr;
+}
+
+char* StrCat(char* dst, const char* src)
+{
+	char* out_arr;
+	size_t i, j, out_arr_size, left_size, right_size;
+	
+	assert(NULL != dst && NULL != src );
+	
+	left_size = StrLen(dst);
+	right_size = StrLen(src);
+	out_arr_size =  left_size + right_size + 1;
+	out_arr = (char*)malloc(sizeof(char) * out_arr_size);
+	
+	for (i = 0; i < left_size; ++i)
+	{
+		out_arr[i] = dst[i];
+	}
+	
+	for (j = 0; i < right_size; ++j, ++i)
+	{
+		out_arr[i] = src[j];
+	}
+	
+	out_arr[i] = '\0';
+	
+	return out_arr;
+}
+
+char* StrDup(const char* str)
+{
+	char* out_arr = NULL;
+	
+	assert(NULL != str );
+	
+	out_arr = (char*)malloc(sizeof(char) * (StrLen(str) + 1));
+	if (NULL == out_arr)
+	{
+		return NULL;
+	}
+	
+	return StrCpy(out_arr, (char*)str);
+}
+
+char makeLower(char ch)
+{
+	return ch + 32;
+}
+
+int CharCmp(char ch, char ch1)
+{
+	if (ch == ch1)
+	{
+		return 0;
+	}
+	if (ch < ch1)
+	{
+		return -1;
+	}
+	
+	return 1;
+}
+
+char* StrChr(const char* str, int c)
+{
+	size_t i;
+	
+	assert(NULL != str && c > 0);
+	
+	for (i = 0; '\0' != str[i]; ++i)
+	{
+		if (str[i] == c)
+		{
+			return (char*)&str[i];
+		}
+	}
+	
+	return (char*)&str[i];
+}
+
+int StrNCmp(const char* s1, const char* s2, size_t n)
+{
+	size_t i;
+	int curent_res;
+	assert(NULL != s1 && NULL != s2);
+	
+	for (i = 0; '\0' != s1[i] && '\0' != s2[i] && i < n; ++i)
+	{
+		curent_res = CharCmp(s1[i], s2[i]);
+		if (0 == curent_res)
+		{
+			continue;
+		}
+		
+		return curent_res;
+	}
+	
+	return 0;
+}
+
+int StrCaseCmp(const char* s1, const char* s2)
+{
+	size_t i;
+	int curent_res;
+	char left, right;
+	
+	assert(NULL != s1 && NULL != s2);
+	
+	for (i = 0; '\0' != s1[i] && '\0' != s2[i]; ++i)
+	{
+		if(s1[i] >= 'A' && s1[i] <= 'Z')
+		{
+			left = makeLower(s1[i]);
+		}
+		else
+		{
+			left = s1[i];
+		}
+		if(s2[i] >= 'A' && s2[i] <= 'Z')
+		{
+			right = makeLower(s2[i]);
+		}
+		else
+		{
+			right = s2[i];
+		}
+		
+		curent_res = CharCmp(left, right);
+		if (0 == curent_res)
+		{
+			continue;
+		}
+		
+		return curent_res;
+	}
+	
+	if ('\0' != s1[i])
+	{
+		return 1;
+	}
+	if ('\0' != s2[i])
+	{
+		return -1;
+	}
+	
+	return 0;
+}
+
+
+char* StrNCpy(char* dst, const char* src, size_t n_bytes)
+{
+	size_t i;
+	
+	assert(NULL != dst && NULL != src);
+	
+	for (i = 0; i < n_bytes && '\0' != src[i] && '\0' != dst[i]; i++)
+	{
+		dst[i] = src[i];
+	}
+	
+	while (dst[i] != '\0')
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	
+	return dst;
+}
+char* StrCpy(char* dst, char* src)
+{
+	size_t i;
+	
+	assert(NULL != dst && NULL != src);
+	
+	for (i = 0; '\0' != src[i]; i++)
+	{
+		dst[i] = src[i];
+	}
+	
+	dst[i] = '\0';
+	
+	return &dst[i];
+}
+
+
+size_t StrLen(const char *str)
+{
+	const char* counter;
+	size_t i;
+	
+	assert(NULL != str);
+	
+	counter = str;
+	for (i = 0; '\0' != str[i]; ++i)
+	{
+		counter++;
+	}
+	
+	return counter - str;
+}
+
+int StrCmp(const char *s1, const char *s2)
+{
+	size_t i;
+	int curent_res;
+	assert(NULL != s1 && NULL != s2);
+	
+	for (i = 0; '\0' != s1[i] && '\0' != s2[i]; ++i)
+	{
+		curent_res = CharCmp(s1[i], s2[i]);
+		if (0 == curent_res)
+		{
+			continue;
+		}
+		
+		return curent_res;
+	}
+	
+	if ('\0' != s1[i])
+	{
+		return 1;
+	}
+	if ('\0' != s2[i])
+	{
+		return -1;
+	}
+	
+	return 0;
+}
+
+
---
+++ c/pointers2/String/test_String.c
@@ -0,0 +1,331 @@
+#include <assert.h>    /* for asserts using */
+#include <stdio.h>     /* for using prints */
+#include <string.h>    /* for strlen, strcmp, etc.. */
+#include <stdlib.h>    /* for malloc alocate */
+#include "String.h" /* for our functions */
+
+void Print_Test_Start(char* func_name);
+void Print_Test_End(char* func_name);
+void resetArray(int* arr, size_t size);
+void printArray(const char* arr, size_t size);
+
+void test_StrLen(void);
+void test_StrCmp(void);
+void test_StrNCmp(void);
+void test_StrCpy(void);
+void test_StrNCpy(void);
+void test_StrCaseCmp(void);
+void test_StrChr(void);
+
+int main()
+{
+	test_StrLen();
+	test_StrCmp();
+	test_StrCpy();
+	test_StrNCpy();
+	test_StrCaseCmp();
+	test_StrChr();
+	return 0;
+}
+
+void test_StrChr(void)
+{
+	char arrHello[] = "Hello";
+	
+	Print_Test_Start("StrChr");
+	if(StrChr(arrHello, arrHello[2]) != &arrHello[2])
+	{
+		printf("fail test StrChr for StrChr(arrHello, arrHello[2]) compere\n");
+		return;
+	}
+	
+	if(NULL != StrChr(arrHello, 'a'))
+	{
+		printf("fail test StrChr for input: StrChr(arrHello, 'a') compere\n");
+		return;
+	}
+	
+	Print_Test_End("StrChr");
+}
+
+void test_StrCaseCmp()
+{
+	char arrHello[] = "Hello";
+	size_t i, size = 6;
+	char* arr1 = (char*)malloc(sizeof(char) * size);
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr1[i] = arrHello[i];
+		arr2[i] = arrHello[i];
+	}
+	
+	Print_Test_Start("StrCaseCmp");
+	if ( 0 != StrCaseCmp(arr1, arr2))
+	{
+		printf("fail test StrCaseCmp for first compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr1[3] = 'a';
+	arr2[3] = 'A';
+	if ( 0 != StrCaseCmp(arr1, arr2))
+	{
+		printf("fail test StrCaseCmp for second compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr2[3] = 'b';
+	if ( -1 != StrCaseCmp(arr1, arr2))
+	{
+		printf("fail test StrCaseCmp for negetive compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr1[3] = 'z';
+	if ( 1 != StrCaseCmp(arr1, arr2))
+	{
+		printf("fail test StrCaseCmp for positive compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	Print_Test_End("StrCaseCmp");
+	
+	free(arr1);
+	free(arr2);
+
+}
+
+void test_StrNCmp(void)
+{
+	char arrHello[] = "Hello";
+	size_t i, size = 6;
+	char* arr1 = (char*)malloc(sizeof(char) * size);
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr1[i] = arrHello[i];
+		arr2[i] = arrHello[i];
+	}
+	
+	arr2[3] = arr2[3] + 1;
+	Print_Test_Start("StrNCmp");
+	if ( 0 != StrNCmp(arr1, arr2, 3))
+	{
+		printf("fail test StrNCmp for first compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	if ( -1 != StrNCmp(arr1, arr2, 4))
+	{
+		printf("fail test StrNCmp for negetive compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr1[3] = arr2[3];
+	if ( 0 != StrNCmp(arr1, arr2, 7))
+	{
+		printf("fail test StrNCmp for last compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	Print_Test_End("StrNCmp");
+	
+	free(arr1);
+	free(arr2);
+	
+}
+void test_StrNCpy(void)
+{
+	const char arr1[] = "Hello";
+	size_t i, size = 6;
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr2[i] = arr1[i];
+	}
+	
+	Print_Test_Start("StrNCpy");
+	StrNCpy(arr2, arr1, size);	
+	if ( 0 != strcmp(arr2, arr1))
+	{
+		printf("fail test StrCpy for first copy\n");
+		printArray( arr1, size);
+		printArray( arr2, size);
+		free(arr2);
+		return;
+	}
+
+	free(arr2);
+	Print_Test_End("StrNCpy");
+}
+
+
+void test_StrCpy(void)
+{	
+	char arrHello[] = "Hello";
+	size_t i, size = 6;
+	char* arr1 = (char*)malloc(sizeof(char) * size);
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr1[i] = arrHello[i];
+	}
+	
+	Print_Test_Start("StrCpy");
+	StrCpy(arr2, arr1);
+	if ( 0 != strcmp(arr2, arr1))
+	{
+		printf("fail test StrCpy for first copy\n");
+		printArray( arr1, size);
+		printArray( arr2, size);
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	Print_Test_End("StrCpy");
+}
+
+void test_StrCmp(void)
+{
+	char arrHello[] = "Hello";
+	size_t i, size = 6;
+	char* arr1 = (char*)malloc(sizeof(char) * size);
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr1[i] = arrHello[i];
+		arr2[i] = arrHello[i];
+	}
+	
+	Print_Test_Start("StrCmp");
+	if ( 0 != StrCmp(arr1, arr2))
+	{
+		printf("fail test StrCmp for first compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr1[3] = 'a';
+	if ( -1 != StrCmp(arr1, arr2))
+	{
+		printf("fail test StrCmp for negetive compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	arr1[3] = 'z';
+	if ( 1 != StrCmp(arr1, arr2))
+	{
+		printf("fail test StrCmp for positive compere\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	Print_Test_End("StrCmp");
+	
+	free(arr1);
+	free(arr2);
+}
+
+
+void test_StrLen(void)
+{
+	char arrHello[] = "Hello";
+	size_t i, size = 6;
+	char* arr1 = (char*)malloc(sizeof(char) * size);
+	char* arr2 = (char*)malloc(sizeof(char) * size);
+	
+	assert(NULL != arr1 && NULL != arr2);
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr1[i] = arrHello[i];
+		arr2[i] = arrHello[i];
+	}
+	
+	Print_Test_Start("StrLen");
+	if (5 != StrLen(arr1))
+	{
+		printf("fail test strLen with with promt arr1\n");
+		free(arr1);
+		free(arr2);
+		return;
+	}
+	
+	free(arr1);
+	free(arr2);
+	Print_Test_End("StrLen");
+}
+
+
+
+
+
+
+
+void resetArray(int* arr, size_t size)
+{
+	size_t i;
+	
+	for (i = 0; i < size; ++i)
+	{
+		arr[i] = 0;	
+	}
+}
+
+void printArray(const char* arr, size_t size)
+{
+	size_t i;
+	
+	for(i = 0; i < size; ++i)
+	{
+		printf("%c, ", arr[i]);
+	}
+	
+	printf("\n");
+}
+
+void Print_Test_Start(char* func_name)
+{
+	printf("--------------------------------------------------------------\n");
+	printf("start testing for: \"%s()\" \n", func_name);	
+}
+void Print_Test_End(char* func_name)
+{
+	printf("Done SECCSFUL testing for: \"%s()\" \n", func_name);
+	printf("--------------------------------------------------------------\n");	
+}
