8e30376792fd7b1bf5521ceb188c21ea67f53545
reuven.bar <1robib1@gmail.com>
2025-12-15 12:42:47 +0200

    first finish all functions, before gc and tests pass

--- c/files/logger.c
+++ c/files/logger.c
@@ -1,10 +1,16 @@
-#include <stddef.h> /* size_t */
-#include <unistd.h> /* For using getcwd (user path to create there the file) */
-#include <stdio.h>  /* for fgets() */
+#include <stddef.h> 	   /* size_t */
+#include <unistd.h> 	  /* For using getcwd (user path to create there the file) */
+#include <stdio.h>  	 /* for fgets() */
+
+#include "String.h"     /* For StrNCmp */
+#include "pointers2.h" /* for DeleteWhiteSpaces */
 
 #define cmd_size 5
-#define True 'T'
-#define False 0; 
+#define TRUE 'T'
+#define FALSE 0; 
+
+char input[BUFSIZ];
+char to_exit = FALSE;
 
 typedef void (*print_fun)(int);
 
@@ -30,6 +36,7 @@ struct print_me
     print_fun operate; 
 };
 
+/*---------------------------- not review functions ---------------------------- */
 void StructInitAndPrint(void)
 {
 	size_t size = 10, i = 0;
@@ -53,7 +60,41 @@ void StructInitAndPrint(void)
 	
 	free(arr); arr = NULL;
 }
+/*---------------------------- end not review functions ---------------------------- */
 
+/*---------------------------- action commends functions ---------------------------- */
+static logger_status_t LogerAppend(const char* file_name)
+{
+	File* file = NULL;
+	
+	assert(NULL != file_name);
+	
+	if (NULL == (file = fopen(file_name, "a")))
+	{
+		return FILE_OPEN_ERROR;
+	}
+	
+	if (EOF == fputs(input, file))
+	{
+		fclose(file);
+		return FILE_WRITE_ERROR; 
+	}
+	
+	if(fclose(file) == EOF)
+	{
+		return FILE_CLOSE_ERROR;
+	}
+		
+	return SUCCESS;
+}
+
+
+static logger_status_t LogerExit(const char* file_name) /* TODO- need free malocs*/
+{
+	to_exit = TRUE;
+	
+	return SUCCESS;
+}
 static logger_status_t LogerRemove(const char* file_name)
 {
 	assert(NULL != file_name);
@@ -100,86 +141,177 @@ static logger_status_t LogerCount(const char* file_name)
 	}
 }
 
-static void OpenPrint(void)
+/*---------------------------- end action commends functions ---------------------------- */
+
+static logger_status_t CopyFileToFile(FILE* src, FILE* dst)
 {
-	printf("Welcome to Mini Loger\n");
-	printf("please enter your input:\n");
-	printf("------------------------------------------------------------\n");
-	printf("'-exit'/'-count'/'-remove'/'<'string''/'string':\n");
-	printf("------------------------------------------------------------\n");
+	char buffer[BUFSIZ];
+	
+	assert(NULL != src);
+	assert(NULL != dst);
+	
+	while (NULL != fgets(buffer, BUFSIZ, src)) 
+    {
+        fputs(buffer, dst);
+    }
+    
+    return SUCCESS;
 }
 
-void PrependToTextFile(const char *filename, const char *string_to_prepend) 
+static logger_status_t LogerHeadAppend(const char *filename) 
 {
-    FILE *original_file NULL;
+    FILE *original_file = NULL;
     FILE *temp_file = NULL;
-    char buffer[];
-    const char *temp_filename = "temp_text.tmp";
+    logger_status_t check = SUCCESS;
+    //char buffer[BUFSIZ];
+    const char *temp_filename = "temp_text.txt";
 	
 	assert(NULL != file_name);
-	assert(NULL != string_to_prepend);
 	
     original_file = fopen(filename, "r");
-    temp_file = fopen(temp_filename, "w");
+    temp_file = fopen(temp_filename, "w+");
 
-    if (original_file == NULL || temp_file == NULL) 
+    if (NULL == original_file || NULL == temp_file) 
     {
-        perror("Error opening files");
-        if (original_file) fclose(original_file);
-        if (temp_file) fclose(temp_file);
-        return;
+        if (original_file)
+        {
+        	fclose(original_file);
+        }
+        if (temp_file)
+        {
+        	fclose(temp_file);
+        }
+        
+        return FILE_OPEN_ERROR;
     }
-
-    fprintf(temp_file, "%s", string_to_prepend);
-
-    while (NULL != fgets(buffer, MAX_LINE_LENGTH, original_file)) 
-    {
-        fputs(buffer, temp_file);
-    }
-
-    fclose(original_file);
-    fclose(temp_file);
-
-    remove(filename);
-    rename(temp_filename, filename);
+	
+	fputs(input, temp_file);
+	check = CopyFileToFile(original_file, temp_file);
+	if (SUCCESS != check)
+	{
+		fclose(original_file);
+		fclose(temp_file);
+		return check;
+	}
     
+    if(EOF == fclose(original_file))
+	{
+		return FILE_CLOSE_ERROR;
+	}
+	
+	original_file = fopen(filename, "w");
+	if (NULL == original_file) 
+    {
+    	fclose(temp_file);
+    	
+        return FILE_OPEN_ERROR;
+    }
+    
+    check = CopyFileToFile(temp_file, original_file);
+    if (SUCCESS != check)
+	{
+		fclose(original_file);
+		fclose(temp_file);
+		return check;
+	}
+	
+    if(EOF == fclose(original_file))
+	{
+		return FILE_CLOSE_ERROR;
+	}
+	if(EOF == fclose(temp_file))
+	{
+		return FILE_CLOSE_ERROR;
+	}
+	
+    remove(temp_filename);
     printf("Prepended text to '%s'.\n", filename);
+    
+    return SUCCESS;
 
+/*---------------------------- Compre functions ---------------------------- */
 char IsCount(const char* line)
-{
-	StrNCmp(input)
+{	
+	char string1_cmp[] = "-count ";
+	char string2_cmp[] = "-count\n";
+	
+	assert(NULL != line);
+	
+	if (0 == StrNCmp(line, string1_cmp, sizeof(string1_cmp)) || 
+		0 == StrNCmp(line, string2_cmp, sizeof(string1_cmp)))
+	{
+		return TRUE;
+	}
+	
+	return FALSE;
 }
 
 char IsExit(const char* line)
 {
+	char string1_cmp[] = "-exit ";
+	char string2_cmp[] = "-exit\n";
 	
+	assert(NULL != line);
+	
+	if (0 == StrNCmp(line, string1_cmp, sizeof(string1_cmp)) || 
+		0 == StrNCmp(line, string2_cmp, sizeof(string1_cmp)))
+	{
+		return TRUE;
+	}
+	
+	return FALSE;
 }
 
 char IsRemove(const char* line)
-{
+{	
+	char string1_cmp[] = "-remove ";
+	char string2_cmp[] = "-remove\n";
 	
+	assert(NULL != line);
+	
+	if (0 == StrNCmp(line, string1_cmp, sizeof(string1_cmp)) || 
+		0 == StrNCmp(line, string2_cmp, sizeof(string1_cmp)))
+	{
+		return TRUE;
+	}
+	
+	return FALSE;
 }
 
 char IsFirstLine(const char* line)
 {
+	char string1_cmp[] = "< ";
+	char string2_cmp[] = "<\n";
 	
+	assert(NULL != line);
+	
+	if (0 == StrNCmp(line, string1_cmp, sizeof(string1_cmp)) || 
+		0 == StrNCmp(line, string2_cmp, sizeof(string1_cmp)))
+	{
+		return TRUE;
+	}
+	
+	return FALSE;
 }
 
 char IsAppend(const char* line)
 {
-	return True;
+	return TRUE;
 }
 
+/*---------------------------- end Compre functions ----------------------------*/
+
+/*-------------------------------- Helper functions ----------------------------*/
 static cmd* initStrctCmd(void)
 {
 	size_t cmd_index = 0;
 	cmd* arr = (cmd*)malloc(sizeof(cmd) * cmd_size);
 	
-	cmd count = {"-count", IsCount, LogerCount};
-	cmd exit = {"-exit", IsExit, /* enter exit function */};
-	cmd remove = {"-remove", IsRemove, LogerRemove};
-	cmd first_line = {"<", IsFirstLine, /* enter first_line function */};
-	cmd append = {"append(defult)", IsAppend , /* enter first_line function */};
+	cmd count = {"-count ", IsCount, LogerCount};
+	cmd exit = {"-exit ", IsExit, LogerExit};
+	cmd remove = {"-remove ", IsRemove, LogerRemove};
+	cmd first_line = {"< ", IsFirstLine, LogerHeadAppend};
+	cmd append = {"append(defult)", IsAppend , LogerAppend};
 	
 	
 	arr[0] = count;	  	
@@ -193,37 +325,58 @@ static cmd* initStrctCmd(void)
 
 static void ActiveCmd(cmd* comend, const char* file_name)
 {
-	if (SUCCESS == comend.cmd_active(file_name))
-	{
-		print("SUCCESS");
+	logger_status_t status = SUCCESS;
+	
+	assert(NULL != file_name);
+	
+	status = comend.cmd_active(file_name);
+	if (SUCCESS == status)
+	{	
+		print("SUCCESS");	
 	}
+	
+	return status;
 }
+
+static void OpenPrint(void)
+{
+	printf("Welcome to Mini Loger\n");
+	printf("please enter your input:\n");
+	printf("------------------------------------------------------------\n");
+	printf("'-exit'/'-count'/'-remove'/'<'string''/'string':\n");
+	printf("------------------------------------------------------------\n");
+}
+
+/*--------------------------------end  Helper functions ----------------------------*/
+
+/*--------------------------------main logger function----------------------------*/
 logger_status_t Logger(const char* file_name)
 {
 	unsigned char user_want_to_keep_progrem = 1;
-	char input[BUFSIZ];
 	cmd* arr = NULL, start_arr = NULL;
 	
 	assert(NULL != file_name);
 	
 	arr = initStrctCmd();
 	start_arr = arr;
-	while(user_want_to_keep_progrem)
+	while(to_exit)
 	{
 		OpenPrint();
 		arr = start_arr;
 		fgets(input, sizeof(input), stdin);
-		/* clean white space; */
-		
+		input = RemoveWhiteSpaces(input);
 		while(True)
 		{	
-			if (False != *arr.cmp_func(*arr.cmd_string))
+			if (FALSE != (*arr).cmp_func(*arr.cmd_string))
 			{
-				ActiveCmd(*arr, file_name);
+				if (SUCCESS == ActiveCmd(*arr, file_name));
 				break;
 			}
 			
 			++arr;
 		}
 	}
+	
+	
 }
+/*--------------------------------end main logger function--------------------*/
--- c/files/logger.h
+++ c/files/logger.h
@@ -1,12 +1,8 @@
 #ifndef __LOGGER_H__
 #define __LOGGER_H__
 
-#include <stddef.h> /* size_t */
+typedef enum {SUCCESS, FILE_OPEN_ERROR, FILE_CLOSE_ERROR, FILE_REMOVE_ERROR, FILE_WRITE_ERROR, FILE_READ_ERROR} logger_status_t;
 
-typedef enum {SUCCESS, FILE_OPEN_ERROR, FILE_WRITE_ERROR} logger_status_t;
-
-
-/*documentation TODO*/
 /** @desc Simple interactive logger which writes user input into text file.
  *  
  *  The function opens or creates a text file specified by @p file_name and continuously
@@ -17,13 +13,16 @@ typedef enum {SUCCESS, FILE_OPEN_ERROR, FILE_WRITE_ERROR} logger_status_t;
  *  - "-count" - prints number of lines in text file 
  *  - "-exit" - close the program
  *  - "-remove" - delete the created file_name.txt
- *  - "<" - write to the begining of the text file
+ *  - "<" - write to the start of the text file
  *  - By default appends the text to the end of file_name.txt
  * 
  * @param[in] file_name - file name of the desired text file
  * @return logger_status_t - returns logger_status_t enum that indicates exit status
  * @errors: FILE_OPEN_ERROR - Indicates file opening was unsuccessful.
+ *          FILE_CLOSE_ERROR - Indicates file closing was unsuccessful.
+ *          FILE_REMOVE_ERROR - Indicates file deleting was unsuccessful.
  *          FILE_WRITE_ERROR - Indicates file writing was unsuccessful.
+ *          FILE_READ_ERROR - Indicates file reading was unsuccessful.
  * 
  * **/
 logger_status_t Logger(const char* file_name);
