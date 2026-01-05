2851e77f3256432b21f816a87e7100222337865c
reuven.bar <1robib1@gmail.com>
2026-01-05 16:08:10 +0200

    need test the circular

---
+++ ds/include/circular_buffer.h
@@ -0,0 +1,4 @@
+#ifndef __CIRCULAR_BUFFER_H__
+#define __CIRCULAR_BUFFER_H__
+
+#endif /* __CIRCULAR_BUFFER_H__ */
---
+++ ds/test/test_circular_buffer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "circular_buffer.h"
+
+#define GREEN "\033[0;32m"
+#define RED   "\033[0;31m"
+#define RESET "\033[0m"
+
+int main() {
+    printf("something " GREEN "passed" RESET "\n");
+    printf("something " RED "failed" RESET "\n");
+    return 0;
+}
