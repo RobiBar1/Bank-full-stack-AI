92e9086a7d2b9b36fea363cc7b89e9e7f20a5086
reuven.bar <1robib1@gmail.com>
2026-01-19 14:26:32 +0200

    finished all pid implements and tests and functions pass tests. TODO if have time can change the create tests for not only prints.

--- ds/include/pid.h
+++ ds/include/pid.h
@@ -1,4 +1,37 @@
-#ifndef __PID_H__
-#define __PID_H__
+#ifndef _UID_H_
+#define _UID_H_
 
-#endif /* __PID_H__ */
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+struct ilrd_uid
+{
+    size_t counter;
+    pid_t pid;
+    time_t time;
+    unsigned char ip[16];  
+};
+
+typedef struct ilrd_uid ilrd_uid_t;
+
+extern ilrd_uid_t bad_uid;
+
+/**
+ * @desc - Creates a UID 
+ * @return a uid or bad uid if fails
+ * @time complexity - O(1)
+*/
+ilrd_uid_t ILRDUIDCreate(void);
+
+
+/**
+ * @desc checks if one is equal to other
+ * @param[in] first - the first uid to compare with 
+ * @param[in] second - the other uid to compare with.
+ * @return 1 if the uid are equal, otherwise 0
+ * @complexity: O(1)
+ */
+int IsILRDUIDEqual(const ilrd_uid_t* first, const ilrd_uid_t* second);
+
+#endif /* _UID_H_ */
--- ds/src/pid.c
+++ ds/src/pid.c
@@ -6,23 +6,47 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-typedef struct ilrd_uid
+#include <assert.h>
+
+#include "pid.h"
+
+#define IP_SIZE 16
+
+ilrd_uid_t bad_uid = {0};  
+
+/*
+struct ilrd_uid
 {
     size_t counter;
     pid_t pid;
     time_t time;
     unsigned char ip[16];  
-} ilrd_uid_t;
+};*/
 
-ilrd_uid_t bad_uid = {0};  
+int IsILRDUIDEqual(const ilrd_uid_t* first, const ilrd_uid_t* second)
+{
+	assert (NULL != first);
+	assert (NULL != second);
+	
+	return first->counter == second->counter && first->pid == second->pid && 
+		   first->time == second->time && 0 == 
+		   strcmp((const char*)first->ip, (const char*)second->ip);
+}
 
-ilrd_uid_t CreateUID(void) 
+
+ilrd_uid_t ILRDUIDCreate(void) 
 {
     static size_t counter = 0;
     ilrd_uid_t new_uid = {0};
-    int s = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in a = {AF_INET, 0x5000, {0x08080808}}, b;
-    socklen_t l = sizeof(b);
+    int s = -1;
+	struct sockaddr_in a;
+	struct sockaddr_in b;
+	socklen_t l = sizeof(b);
+
+	a.sin_family = AF_INET;
+	a.sin_port = htons(80);
+	a.sin_addr.s_addr = 0x08080808;
+	memset(a.sin_zero, 0, sizeof(a.sin_zero));
     
     time(&new_uid.time);
     if (-1 == new_uid.time) 
@@ -30,15 +54,34 @@ ilrd_uid_t CreateUID(void)
         return bad_uid;
     }
     
-    ++counter;
-    new_uid.counter = counter;
-    new_uid.pid = getpid();
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (-1 == s)
+    {
+        return bad_uid;
+    }
     
-    connect(s, (void*)&a, sizeof(a));
-    getsockname(s, (void*)&b, &l);
-    close(s); 
+    if (-1 == connect(s, (void*)&a, sizeof(a)))
+    {
+        close(s);
+        return bad_uid;
+    }
+    
+    if (-1 == getsockname(s, (void*)&b, &l))
+    {
+        close(s);
+        return bad_uid;
+    }
+    
+    if (-1 == close(s))
+    {
+        return bad_uid;
+    } 
     
     strcpy((char*)new_uid.ip, inet_ntoa(b.sin_addr));
+    
+    new_uid.pid = getpid();
+    ++counter;
+    new_uid.counter = counter;
     
     return new_uid; 
 }
