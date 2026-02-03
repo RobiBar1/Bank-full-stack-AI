0cb91f1f008537c375ceaf0e6aafb4a4562f1a30
reuven.bar <1robib1@gmail.com>
2026-02-03 18:19:56 +0200

    finished all functions, pass all tests, pass vlg.

--- ds/include/bst.h
+++ ds/include/bst.h
@@ -6,20 +6,20 @@
 /*
 typedef enum {LEFT, RIGHT, NUM_CHILDREN} children_t;
 
-typedef struct bst_node
+struct bst_node
 {
     void* data;
     bst_node_t* parent;
     bst_node_t* children[NUM_CHILDREN];
-} bst_node_t;
+} 
 */
 
 /*
-typedef struct bst
+struct bst
 {
     bst_node_t* end;
     int (*cmp)(const void* data, const void* param);
-}
+};
 */
 
 typedef struct bst_node* bst_iter_t;
@@ -31,13 +31,12 @@ typedef struct bst bst_t;
  * @param cmp - compare function for sorting the elements in the list
  * The comparison function must return an integer less than, equal to, or
  * greater than zero if the first argument is considered to be respec‐
- * tively less than, equal to, or greater than the second. If two members
- * compare as equal, their order in the sorted array will be positionally stable.
+ * tively less than, equal to, or greater than the second. 
  * @pre cmp != NULL
- * @returns pointer to the new tree, or NULL if allocation fails
+ * @returns pointer to the new tree, or NULL on failure.
  * @complexity O(1)
  */
-bst_t* BSTCreate(int(*cmp)(const void* data, const void* param));
+bst_t* BSTCreate(int(*cmp)(const void* left, const void* right)); 
 
 /**
  * @desc - destroys the bst
@@ -49,11 +48,11 @@ bst_t* BSTCreate(int(*cmp)(const void* data, const void* param));
 void BSTDestroy(bst_t* bst);
 
 /**
- * @desc insert element in the bst
+ * @desc insert element in the bst, cannot insert the same data twice-
  * @param[in] bst - the bst to add the element to
  * @param[in] data - the data to insert
  * @pre bst != NULL
- * @return bst_iter_t - the iterator that added to the tree
+ * @return bst_iter_t - an iterator to the added data or dummy if failed. 
  * @complexity: average O(logn), worst case O(n)
  **/
 bst_iter_t BSTInsert(bst_t* bst, const void* data);
@@ -71,18 +70,17 @@ bst_iter_t BSTRemove(bst_iter_t iter);
  * @desc checks if two iterators are equal (are the same iter) to each other
  * @param[in] iter1 - the first iterator
  * @param[in] iter2 - the second iterator
- * @pre - iter1, iter2 are not NULL
  * @return int - 1 if they are equal (are the same iter), 0 if they are not equal
  * @complexity: O(1)
  */
-int BSTIsIterEqual(const bst_iter_t iter1, const bst_iter_t iter2);
-
+int BSTIsIterEqual(bst_iter_t iter1, bst_iter_t iter2);
+    
 /**
  * @desc returns the smallest element in the bst
  * @param[in] bst_t* - pointer to bst
  * @pre bst != NULL;
- * @return bst_iter_t - the smallest element in the bst
- * @complexity: O(1)
+ * @return bst_iter_t - the smallest element in the bst. returns same value as BSTEnd if empty.
+ * @complexity: O(logn)
  */
 bst_iter_t BSTBegin(const bst_t* bst);
 
@@ -98,25 +96,29 @@ bst_iter_t BSTEnd(const bst_t* bst);
 
 /**
  * @desc: returns the previous element  
- * @param[in] the iter pointing to the node we want to get his prev
+ * @param[in] the iter pointing to the node we want to get his prev, can be iterator returned by 
+ * BSTEnd.
  * @returns: the iter to the prev node , undefined if the node is the smallest.
  */
-bst_iter_t BSTGetPrev(const bst_iter_t iter);
+bst_iter_t BSTGetPrev(bst_iter_t iter);
 
 /**
  * @desc: returns the next element in the bst
- * @param[in] the iter pointing to the node we want to the next of
- * @returns: the iter to the next node or BSTEnd if its the end
+ * @param[in] the iter pointing to the node we want to get his next 
+ * @pre iter != iterator returned by BSTGetEnd, undefined behaviour;
+ * @returns: the iter to the next node or BSTEnd if its the biggest
  * @Complexicity: average O(logn), worst O(n)
  */
-bst_iter_t BSTGetNext(const bst_iter_t iter);
+bst_iter_t BSTGetNext(bst_iter_t iter);
 
 /**
  * @desc: returns the data from the given iterator
- * @param[in] the iter pointing to the node we want to get his data
+ * @param[in] the iter pointing to the node we want to get his data 
+ * @iter != iterator returned from BSTGetEnd
  * @returns: void* contains the data 
+ * complexity: O(1)
  */
-void* BSTGetData(const bst_iter_t iter);
+void* BSTGetData(bst_iter_t iter);
 
 /**
  * @desc finds the first occurrence of 'data' in binary
@@ -151,19 +153,18 @@ size_t BSTCount(const bst_t* bst);
 /**
  * @desc applies an action over the range [from, to)
  * @param[in] from - an iterator to start run from
- * @param[in] to - an iterator to end run on (not include that iterator == run until 1 itertator before that).
+ * @param[in] to - an iterator to end run on (not include that iterator == run until 1 itertator before that). / the text in the parenthesis is redundant
  * @param[in] action - a function which executes a modification on the iterator.
  * action should return 0 upon success or a non-zero value upon failure
  * @param[in] param - what action() should be applied onto the iterator value
  * @pre 'from' and 'to' belong to the same bst
  * @pre 'from' position <= 'to' position in the bst
- * @pre action,bst,from,to != NULL
+ * @pre action != NULL
+ * @pre from != iterator returned from BSTGetEnd
  * @return 0 - all actions on iterators were successful, non-zero otherwise
- * @complexity: O(n)
- 
+ * @complexity: O(N)
  */
-int BSTForEach(bst_t* bst, int(*action)(void* data, void* param), bst_iter_t from, bst_iter_t to);
+int BSTForEach(bst_iter_t from, bst_iter_t to, int(*action)(void* data, void* param), void* param);
 
 
-
-#endif /* __BST_H__ */
+#endif /* __BST_H__ */  
--- ds/src/bst.c
+++ ds/src/bst.c
@@ -1,24 +1,36 @@
-#include "bst.h"
+#include <assert.h> /* assert  */
+#include <stdlib.h> /* malloc  */
+
+#include "bst.h" 	/* our api */
 
 #define FOUND_ELEMNT(num) ((num) == 0)
-#define LEFT_BIGGER(num) ((num) < 0) 
-#define RIGHT_BIGGER(num) ((num) > 0) 
+#define LEFT_BIGGER(num) ((num) > 0) 
+#define RIGHT_BIGGER(num) ((num) < 0) 
+#define SUCCESS (0) 
+#define UNUSED(x) ((void)(x))
+
+typedef enum {LEFT, RIGHT, NUM_CHILDREN} children_t;
 
 typedef struct bst_node
 {
     void* data;
-    bst_node_t* parent;
-    bst_node_t* children[NUM_CHILDREN];
+    struct bst_node* parent;
+    struct bst_node* children[NUM_CHILDREN];
 } bst_node_t;
 
-typedef struct bst
+struct bst
 {
     bst_node_t* end;
     int (*cmp)(const void* data, const void* param);
-}
+};
 
-static bst_node_t* CreateBSTNode(void* data, bst_node_t* parent,
-	   bst_node_t* left, bst_node_t* right)
+/*
+typedef struct bst_node* bst_iter_t;
+typedef struct bst bst_t;
+*/
+
+bst_node_t* CreateBSTNode(const void* data, const bst_node_t* parent,
+		   const bst_node_t* left, const bst_node_t* right)
 {
 	bst_node_t* new_bst_node = (bst_node_t*)malloc(sizeof(bst_node_t));
 	if (NULL == new_bst_node)
@@ -26,10 +38,10 @@ static bst_node_t* CreateBSTNode(void* data, bst_node_t* parent,
 		return NULL;
 	}
 	
-	new_bst_node->data = data;
-	new_bst_node->parent = parent;
-	new_bst_node->children[LEFT] = left;
-	new_bst_node->children[RIGHT] = right;
+	new_bst_node->data = (void*)data;
+	new_bst_node->parent = (bst_node_t*)parent;
+	new_bst_node->children[LEFT] = (bst_node_t*)left;
+	new_bst_node->children[RIGHT] = (bst_node_t*)right;
 	
 	return new_bst_node;
 }
@@ -48,7 +60,7 @@ bst_t* BSTCreate(int(*cmp)(const void* data, const void* param))
 	}
 	
 	dummy_node = CreateBSTNode(NULL, NULL, NULL, NULL);
-	if (NULL == new_bst_node)
+	if (NULL == dummy_node)
 	{
 		free(new_bst); new_bst = NULL;
 		return NULL;
@@ -63,7 +75,7 @@ bst_t* BSTCreate(int(*cmp)(const void* data, const void* param))
 void BSTDestroy(bst_t* bst)
 {
 	bst_node_t* current = NULL;
-	bst_node_t* temp = NULL;
+	bst_node_t* tmp = NULL;
 	
 	assert (NULL != bst);
 	
@@ -102,58 +114,70 @@ void BSTDestroy(bst_t* bst)
 	free(bst);
 }
 
-static bst_iter_t ConvertBSTNodeToIter(bst_node* node)
+static bst_iter_t ConvertBSTNodeToIter(bst_node_t* node)
 {
 	return ((bst_iter_t)node);
 }
 
-static bst_node* ConvertIterToBSTNode(bst_iter_t iter)
+static bst_node_t* ConvertIterToBSTNode(bst_iter_t iter)
 {
-	return ((bst_node*)iter);
+	return ((bst_node_t*)iter);
 }
 
-
-bst_node* BSTFindPlace(bst_node** parent, int* side, bst_t* bst, void* data)
+bst_node_t* BSTFindPlace(bst_node_t** parent, 
+						 int* side, bst_t* bst, void* data)
 {
-	int i = 0;
 	int result = 0;
-	bst_node* current = NULL;
-	bst_node* prev = NULL;
+	bst_node_t* current = NULL;
+	bst_node_t* prev = NULL;
 	
 	assert (NULL != bst);
 	
 	current = bst->end->children[LEFT];
+	*parent = bst->end;
+	*side = LEFT;
 	while (NULL != current)
 	{
-		result = bst->cmp(data, current->data);
+		result = bst->cmp(current->data, data);
 		prev = current;
 		if (FOUND_ELEMNT(result))
-		{
-			*parent = current->parent;
-			*side = LEFT_BIGGER(result);
-			
+		{	
 			return current;
 		}
 		else if (RIGHT_BIGGER(result))
 		{
+			*parent = prev;
+			*side = RIGHT;
 			current = current->children[RIGHT];
 		}
 		else
-		{
+		{	
+			*parent = prev;
+			*side = LEFT;
 			current = current->children[LEFT];
 		}
 	}
 	
-	*parent = prev;
-	*side = LEFT_BIGGER(result);
+	return bst->end;
 }
 
-#define LEFT_BIGGER(num) ((num) > 0) 
+bst_iter_t BSTFind(const bst_t* bst, const void* data)
+{
+	bst_node_t* parent = NULL;
+	bst_node_t* current = NULL;
+	int side = 0;
+	
+	UNUSED(parent);
+	UNUSED(current);
+	UNUSED(side);
+	
+	return ConvertBSTNodeToIter(BSTFindPlace(&parent, &side, (bst_t*)bst, (void*)data));
+}
 
 bst_iter_t BSTInsert(bst_t* bst, const void* data)
 {
-	bst_node* parent = NULL;
-	bst_node* current = NULL;
+	bst_node_t* parent = NULL;
+	bst_node_t* current = NULL;
 	int side = 0;
 	
 	assert (NULL != bst);
@@ -170,7 +194,7 @@ bst_iter_t BSTInsert(bst_t* bst, const void* data)
 	}
 	else
 	{
-		BSTFindPlace(&parent, &side, bst, data);
+		BSTFindPlace(&parent, &side, bst, (void*)data);
 		parent->children[side] = CreateBSTNode(data, parent, NULL, NULL);
 		if (NULL == parent->children[side])
 		{
@@ -183,23 +207,229 @@ bst_iter_t BSTInsert(bst_t* bst, const void* data)
 
 bst_iter_t BSTEnd(const bst_t* bst)
 {
-	 assert (NULL != bst);
+	assert (NULL != bst);
 
-	return bst->end;
+	return ConvertBSTNodeToIter(bst->end);
+}
+
+void* BSTGetData(const bst_iter_t iter)
+{
+	bst_node_t* node = ConvertIterToBSTNode(iter);
+	
+	assert (NULL != node);
+	
+	return (node->data);
+}
+
+int BSTIsEmpty(const bst_t* bst)
+{
+	assert (NULL != bst);
+	
+	return (bst->end->children[LEFT] == NULL);
+}
+
+static bst_node_t* GetMostLeft(bst_node_t* current)
+{
+	assert (NULL != current);
+	
+	while (NULL != current->children[LEFT])
+	{
+		current = current->children[LEFT];
+	}
+	
+	return current;
+}
+
+bst_iter_t BSTBegin(const bst_t* bst)
+{	
+	assert (NULL != bst);
+	
+	if (NULL != bst->end->children[LEFT])
+	{
+		return ConvertBSTNodeToIter(GetMostLeft(bst->end));	
+	}
+	else if (NULL != bst->end->children[RIGHT])
+	{
+		return ConvertBSTNodeToIter(GetMostLeft(bst->end->children[RIGHT]));	
+	}
+	
+	return ConvertBSTNodeToIter(bst->end);
+}
+
+int BSTIsIterEqual(const bst_iter_t iter1, const bst_iter_t iter2)
+{
+	bst_node_t* node1 = ConvertIterToBSTNode(iter1);
+	bst_node_t* node2 = ConvertIterToBSTNode(iter2);
+	
+	assert (NULL != node1);
+	assert (NULL != node2);
+	
+	return (node1->data == node2->data);
+}
+
+static bst_node_t* GetMostRight(bst_node_t* current)
+{
+	assert (NULL != current);
+	
+	while (NULL != current->children[RIGHT])
+	{
+		current = current->children[RIGHT];
+	}
+	
+	return current;
+}
+
+static int IsBSTNodeEquals(bst_node_t* left, bst_node_t* right)
+{	
+	return (left == right);
+}
+
+static bst_node_t* GetPrev(bst_node_t* current)
+{
+	assert (NULL != current);
+	
+	if (NULL != current->children[LEFT])
+	{
+		current = current->children[LEFT];
+		return GetMostRight(current);
+	}
+	else
+	{
+		while (IsBSTNodeEquals(current, current->parent->children[LEFT]))
+		{
+			current = current->parent;
+		}
+	}
+	
+	return current->parent;
+}
+
+bst_iter_t BSTGetPrev(bst_iter_t iter)
+{
+	return ConvertBSTNodeToIter(GetPrev(ConvertIterToBSTNode(iter)));
+}
+
+static bst_node_t* GetNext(bst_node_t* current)
+{
+	assert (NULL != current);
+	
+	if (NULL != current->children[RIGHT])
+	{
+		current = current->children[RIGHT];
+		return GetMostLeft(current);
+	}
+	else
+	{
+		while (IsBSTNodeEquals(current, current->parent->children[RIGHT]))
+		{
+			current = current->parent;
+		}
+	}
+	
+	return current->parent;
+}
+
+bst_iter_t BSTGetNext(bst_iter_t iter)
+{
+	return ConvertBSTNodeToIter(GetNext(ConvertIterToBSTNode(iter)));
 }
 
 
+static int IsLeaf(bst_node_t* node)
+{
+	assert (NULL != node);
+	
+	return (NULL == node->children[LEFT] && NULL == node->children[RIGHT]);
+}
 
+static int WhatSideChildAmI(bst_node_t* current)
+{
+	assert (NULL != current);	
+	
+	return (IsBSTNodeEquals(current->parent->children[RIGHT],current) * RIGHT
+		  +IsBSTNodeEquals(current->parent->children[LEFT],current) * LEFT);
+}
 
+static void HelperRemoveForLeafOrOneChild(bst_node_t* current)
+{
+	int side = 0;
+	int child_side = 0;
+	
+	assert (NULL != current);
+	
+	side = WhatSideChildAmI(current);
+	if (IsLeaf(current))
+	{
+		current->parent->children[side] = NULL;
+		free(current); current = NULL;
+	}
+	else
+	{
+		child_side = (NULL == current->children[LEFT] ? RIGHT : LEFT); 
+		current->parent->children[side] = current->children[child_side];
+		current->children[child_side]->parent =  current->parent;
+		free(current); current = NULL;
+	}
+}
 
+bst_iter_t BSTRemove(bst_iter_t iter)
+{
+	bst_node_t* current = ConvertIterToBSTNode(iter);
+	bst_iter_t next = NULL;
+	
+	assert (NULL != current);
+	assert (NULL != current->parent);
+	
+	next = ConvertBSTNodeToIter(GetNext(current));
+	if (NULL == current->children[LEFT] || NULL == current->children[RIGHT])
+	{
+		HelperRemoveForLeafOrOneChild(current);
+		return next;
+	}
+	
+	current->data = (ConvertIterToBSTNode(next))->data;
+	HelperRemoveForLeafOrOneChild(ConvertIterToBSTNode(next));
+	
+	return ConvertBSTNodeToIter(current);
+}
 
+static int counting(void* data, void* param)
+{
+	UNUSED(data);
+	++(*(size_t*)param);
+	
+	return 0;
+}
 
+size_t BSTCount(const bst_t* bst)
+{
+	size_t i = 0;	
+	
+	assert (NULL != bst);
+	
+	BSTForEach(BSTBegin(bst), BSTEnd(bst), counting, &i);
+	
+	return i;
+}
 
-
-
-
-
-
-
-
-
+int BSTForEach(bst_iter_t from, bst_iter_t to, int(*action)(void* data, void* param), void* param)
+{
+	bst_node_t* from_node = ConvertIterToBSTNode(from);
+	bst_node_t* from_to = ConvertIterToBSTNode(to);
+	int status = 0;
+	
+	assert (NULL != action);
+	assert (NULL != from_node);
+	assert (NULL != from_to);
+	
+	for (;!IsBSTNodeEquals(from_node, from_to); from_node = GetNext(from_node))
+	{
+		status = action(from_node->data, param);
+		if (SUCCESS != status)
+		{
+			return status;
+		}
+	}
+	
+	return SUCCESS;
+}
