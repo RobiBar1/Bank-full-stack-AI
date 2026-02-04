cb72a77ece1ff8ddd09d85ce454c2468d69ebb8c
reuven.bar <1robib1@gmail.com>
2026-02-04 14:03:09 +0200

    add counter to tests and summarize tets results

--- ds/test/test_FSM.c
+++ ds/test/test_FSM.c
@@ -18,21 +18,25 @@ static void PrintEnd(char* name_function)
 	printf("================================================================\n");
 }
 
-int main() 
+static void TestIsValidString(void)
 {
-  	char* test[] = {"011010", "0", "01", "1", "10", "011101", "00", "000000000", "00nooneher00", "111111111", "1111000"};
+	char* test[] = {"011010", "0", "01", "1", "10", "011101", "00", "000000000", "00nooneher00", "111111111", "1111000"};
   	int expeted_result[] = {1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0};
   	int real_result = 0;
+  	int total_tests = 0;
+  	int total_tests_pass = 0;
 	size_t i = 0;
 	
 	PrintStart("IsValidString");
 	for (; i < 11; ++i)
 	{
 		real_result = IsValidString(test[i]);
+		++total_tests;
 		printf("input \"%s\" is: %s ", test[i], real_result ? GREEN "VALID " RESET : RED "NOT VALID " RESET);
 		if (real_result == expeted_result[i])
 		{
 			printf(GREEN "Correct Answer!\n" RESET);
+			++total_tests_pass;
 		}
 		else
 		{
@@ -42,6 +46,14 @@ int main()
 	}
 	
 	PrintEnd("IsValidString");
+	
+	total_tests == total_tests_pass ? printf(GREEN) : printf(RED);
+	printf("you pass: %d/%d tests.\n" RESET, total_tests_pass, total_tests);	
+}
+
+int main() 
+{
+	TestIsValidString();
 	
 	return 0;
 }
