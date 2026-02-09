88dbf5e5cdc8cdcb9132c03e33801afaeea6e9a9
reuven.bar <1robib1@gmail.com>
2026-02-09 13:10:06 +0200

    started sorting with stacks

--- ds/src/recursion.c
+++ ds/src/recursion.c
@@ -1,1 +1,35 @@
 #include "recursion.h"
+
+static SortStack1(stack_t* s, int cur_num)
+{
+	int tmp = 0;
+	
+	assert (NULL != s);
+	
+	if (!StackIsEmpty(s))
+	{
+		tmp = StackPeek(s, &tmp);
+	}
+	
+	
+}
+
+static SortStack2(stack_t* stack, size_t size)
+{
+	assert (NULL != stack);
+}
+
+stack_t* SortStack(stack_t* s)
+{
+	int tmp = 0;
+	
+	assert (NULL != stack);
+	
+	if (!StackIsEmpty(s))
+	{
+		tmp = StackPeek(s, &tmp);
+		SortStack1(s, StackSize(s), tmp);
+	}
+	
+	return s;
+}
