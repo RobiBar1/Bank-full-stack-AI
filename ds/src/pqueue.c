eb7daaa6f8e0ff91016593a45875728418a5a094
reuven.bar <1robib1@gmail.com>
2026-02-24 12:04:06 +0200

    change pq to implement to use heap in him.

--- ds/src/pqueue.c
+++ ds/src/pqueue.c
@@ -4,111 +4,98 @@ Checker: Omer
 Date: 	 21.01.2026
 */
 
-#include <assert.h> 	  /* assert 			 */
-#include <stdlib.h> 	  /* malloc, free 		 */
-#include <stdio.h>  	  /* printf 			 */
+#include <assert.h> /* assert 			         */
+#include <stdio.h>  /* printf 			         */
+#include <stdlib.h> /* malloc, free 		     */
 
-#include "pqueue.h"       /* our api 			 */
-#include "sorted_list.h"  /* our sorted_list api */
+#include "heap.h"   /* our heap api  			 */
+#include "pqueue.h" /* our api 			 		 */
 
-struct pqueue 
+struct pqueue
 {
-	sorted_list_t* sorted_list;
+    heap_t* heap;
 };
 
 pqueue_t* PQueueCreate(priority_cmp_t func)
 {
-	pqueue_t* pq = NULL;
-	
-	assert (NULL != func);
-	
-	pq = (pqueue_t*)malloc(sizeof(pqueue_t));
-	if (NULL == pq)
-	{
-		return NULL;
-	}
-	
-	pq->sorted_list = SortedLCreate(func);
-	if (NULL == pq->sorted_list)
-	{
-		free(pq); pq = NULL;
-		return NULL;
-	}
-	
-	return pq;
+    pqueue_t* pq = NULL;
+
+    assert(NULL != func);
+
+    pq = (pqueue_t*)malloc(sizeof(pqueue_t));
+    if (NULL == pq)
+    {
+        return NULL;
+    }
+
+    pq->heap = HeapCreate(1, func);
+    if (NULL == pq->heap)
+    {
+        free(pq);
+        pq = NULL;
+        return NULL;
+    }
+
+    return pq;
 }
 
 void PQueueDestroy(pqueue_t* que)
 {
-	assert (NULL != que);
-	
-	SortedLDestroy(que->sorted_list);
-	que->sorted_list = NULL;
-	free(que); que = NULL;
+    assert(NULL != que);
+
+    HeapDestroy(que->heap);
+    que->heap = NULL;
+    free(que);
+    que = NULL;
 }
 
 int PQueueEnqueue(pqueue_t* que, const void* data)
 {
-	assert (NULL != que);
-	
-	return (SortedLIsIterEqual(SortedLEnd(que->sorted_list),
-										SortedLInsert(que->sorted_list, data)));
+    assert(NULL != que);
+
+    return HeapPush(que->heap, data);
 }
 
 int PQueueIsEmpty(const pqueue_t* que)
 {
-	assert (NULL != que);
-	
-	return SortedLIsEmpty(que->sorted_list);
+    assert(NULL != que);
+
+    return HeapIsEmpty(que->heap);
 }
 
 void* PQueueDequeue(pqueue_t* que)
 {
-	assert (NULL != que);
-	assert (!PQueueIsEmpty(que));
-	
-	return SortedLPopFront(que->sorted_list);
+    void* data = NULL;
+
+    assert(NULL != que);
+    assert(!HeapIsEmpty(que->heap));
+
+    data = HeapPeek(que->heap);
+    HeapPop(que->heap);
+
+    return data;
 }
 
 void* PQueuePeek(const pqueue_t* que)
 {
-	assert (NULL != que);
-	assert (!PQueueIsEmpty(que));
-	
-	return SortedLGetData(SortedLBegin(que->sorted_list));
+    assert(NULL != que);
+    assert(!HeapIsEmpty(que->heap));
+
+    return HeapPeek(que->heap);
 }
 
 size_t PQueueSize(const pqueue_t* que)
 {
-	assert (NULL != que);
-	
-	return SortedLCount(que->sorted_list);
+    assert(NULL != que);
+
+    return HeapCount(que->heap);
 }
 
-void* PQueueRemove(pqueue_t* que,const void* param, int (*is_match)(const void* data, const void* param))
+void* PQueueRemove(pqueue_t* que, const void* param,
+                   int (*is_match)(const void* data, const void* param))
 {
-	sorted_list_iter_t to = {0}; 
-	sorted_list_iter_t result = {0}; 
-	void* data = NULL;
-	
-	assert (NULL != que);
-	assert (!PQueueIsEmpty(que));
-	assert (NULL != is_match);
-	
-	to = SortedLEnd(que->sorted_list);
-	result = SortedLFindIf(SortedLBegin(que->sorted_list), to, is_match, param);
-	if (SortedLIsIterEqual(to, result))
-	{
-		return NULL;
-	}
-	
-	data = SortedLGetData(result);
-	SortedLRemove(result);
-	
-	return data;
-}
+    assert(NULL != que);
+    assert(NULL != is_match);
 
-int is_match(const void* one, const void* two)
-{
-	return (*(int*)one - *(int*)two);	
+    return HeapRemove(que->heap, param, is_match);
 }
